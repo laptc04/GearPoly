9d7fc00a3fa07da664188a9afde962e9e3209cf6
laptc04 <conglapp04@gmail.com>
2024-06-05 09:35:30 +0700

    ASM Java5

---
+++ src/main/java/com/fpoly/sd18306/AsmJava5GearPolyApplication.java
@@ -0,0 +1,13 @@
+package com.fpoly.sd18306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmJava5GearPolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmJava5GearPolyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpoly/sd18306/AsmJava5GearPolyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpoly.sd18306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmJava5GearPolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
