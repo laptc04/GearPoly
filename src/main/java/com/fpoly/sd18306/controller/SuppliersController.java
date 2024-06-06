79bb0862cbd6da2f83f5776888eb74eeadab8714
phuntpc06376 <148943202+phuntpc06376@users.noreply.github.com>
2024-06-06 10:58:53 +0700

    SuppliersController(hangsanxuat)

---
+++ src/main/java/com/fpoly/sd18306/controller/SuppliersController.java
@@ -0,0 +1,18 @@
+package com.fpoly.sd18306.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class SuppliersController {
+	
+	
+	
+	
+	@GetMapping("/Suppliers")
+	public String getSuppliers() {
+		return "admin/qlHangsx";
+	}
+	
+}
