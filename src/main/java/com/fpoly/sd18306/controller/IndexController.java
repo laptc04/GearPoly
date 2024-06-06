04ebd3a4959462d2bc5f5b0355e74303c10bce95
laptc04 <conglapp04@gmail.com>
2024-06-06 10:35:35 +0700

    Sửa 1 xíu

--- src/main/java/com/fpoly/sd18306/controller/IndexController.java
+++ src/main/java/com/fpoly/sd18306/controller/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/user/")
 public class IndexController {
 
-	@GetMapping("/index")
+	@GetMapping("/index/trangchu")
 	public String getUserIndex() {
 		return "client/index";
 	}
