6d7d45109a0eedfa24c7adf734ad4518feccf5d0
phuntpc06376 <148943202+phuntpc06376@users.noreply.github.com>
2024-06-08 14:49:11 +0700

    đẩy dư liệu sản phẩm vs chi tiết sản phẩm
    đẩy dư liệu sản phẩm vs chi tiết sản phẩm

--- src/main/java/com/fpoly/sd18306/controller/CartController.java
+++ src/main/java/com/fpoly/sd18306/controller/CartController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fpoly.sd18306.jpa.CartJpa;
+import com.fpoly.sd18306.jpa.ProductJPA;
 
 
 
@@ -15,24 +16,35 @@ public class CartController {
 	@Autowired
 	CartJpa cartJpa; 
 	
-	@GetMapping("/cart")
-	public String cartList(Model model) {
-		model.addAttribute("cart", cartJpa.findAll());
-		return "client/index";
-		
-	}
+	@Autowired
+	ProductJPA productJPA;
+	
+//	@GetMapping("/index")
+//	public String cartList(Model model) {
+//		model.addAttribute("cart", cartJpa.findAll());
+//		return "client/index";
+//		
+//	}
 	@GetMapping("user/index")
 	public String getsanpham(Model model) {
-		model.addAttribute("cart", cartJpa.findAll());
+		model.addAttribute("products", productJPA.findAll());
 		return "client/sanpham";
 		
 	}
-//	@GetMapping("/add-to-cart")
-//	public String updateCart(@RequestParam("id") int id , @RequestParam("path") String path,
-//			@RequestParam(name = "quantity", defaultValue = "1") int quantity) {
-//		cartService.add(id);
-//		return String.format("redirect:%s", path);
-//	}
+	
+	@GetMapping("user/chitiet")
+	public String getchitietsanpham(Model model,@RequestParam("id") String id){
+		model.addAttribute("productID", productJPA.findById(id).orElse(null));
+		return "client/chitietsanpham";
+	}
+		
+	
+	@GetMapping("/add-to-cart")
+	public String updateCart(Model model,@RequestParam("id") int id , @RequestParam("path") String path,
+			@RequestParam(name = "quantity", defaultValue = "1") int quantity) {
+		model.addAttribute("cartID", cartJpa.findById("id"));
+		return "client/giohang";
+	}
 //	//
 //	@GetMapping("/remove-to-cart")
 //	public String removeCar(@RequestParam("id") int id) {
